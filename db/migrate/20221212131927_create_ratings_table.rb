b9948953fbf003afbe47d13d6b3fd808b7a2779e
Alina Paik <alinapaik143@gmail.com>
2022-12-12 17:31:44 +0200

    create db

---
+++ db/migrate/20221212131927_create_ratings_table.rb
@@ -0,0 +1,9 @@
+class CreateRatingsTable < ActiveRecord::Migration[5.2]
+  def change
+    create_table :ratings do |t|
+      t.references :user, index: true, foreign_key: true
+      t.references :movie, index: true, foreign_key: true
+      t.integer :grade, null: false, default: 0
+    end
+  end
+end
